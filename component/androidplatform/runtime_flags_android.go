743281a00325e54ec1ad07ba68b853a12fac0f86
qwqgong-ui <qwqgong@gmail.com>
2026-07-23 21:11:45 +0800

    feat(android): register runtime override flags

---
+++ component/androidplatform/runtime_flags_android.go
@@ -0,0 +1,29 @@
+//go:build android
+
+package androidplatform
+
+import "flag"
+
+var activeRuntimeOverrides = RuntimeOverrides{
+	ProcessMatching: true,
+	IPv6Enabled:     true,
+}
+
+func init() {
+	flag.BoolVar(
+		&activeRuntimeOverrides.ProcessMatching,
+		"android-process-matching",
+		true,
+		"enable Android endpoint-aware process matching",
+	)
+	flag.BoolVar(
+		&activeRuntimeOverrides.IPv6Enabled,
+		"android-ipv6",
+		true,
+		"enable IPv6 in the Android runtime configuration",
+	)
+}
+
+func applyRuntimeOverrides(cfg *config.Config) error {
+	return applyRuntimeOverridesForOptions(cfg, activeRuntimeOverrides)
+}
